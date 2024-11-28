c205ed0299a5906cae994f66fbe905ef3b6a70db
Lucas Carvalho <lucas.carvalho.lopes016@gmail.com>
2024-11-28 16:13:56 -0300

    codificando os metodos para o teste do jogo

--- jogodavelha/JogoDaVelha.java
+++ jogodavelha/JogoDaVelha.java
@@ -4,14 +4,63 @@
  */
 package com.lucas.jogodavelha;
 
+import com.lucas.regraseverificacoes.Velha;
+import java.util.Scanner;
+
 /**
  *
  * @author HOME
  */
 public class JogoDaVelha {
     
+    static Scanner scan;
+    static Velha velha;
+    
     public static void main(String[] args){
         
+        scan = new Scanner(System.in);
+        velha = new Velha();
+        
+        boolean ganhou = false;
+        char jogador = 'A';
+        int jogada = 0;
+        
+        while(!ganhou){
+            
+            if(jogada % 2 == 0){
+                System.out.println("Vez do jogador 1");
+                jogador = 'X';
+            }else{
+                System.out.println("Vez do jogador 2");
+                jogador = 'O';
+            }
+            
+            
+            
+        }
+        
+    }
+    
+    public static int valor(String tipoValor){
+        int valor = 0;
+        boolean valorValido = false;
+        
+        while(!valorValido){
+            
+            System.out.println("Digite o valor da " + tipoValor + ": ");
+            valor = scan.nextInt();
+            
+            if(valor >= 1 && valor < 4){
+                valorValido = true;
+            }else{
+                System.out.println("Valor invalido!");
+            }
+            
+        }
+        
+        valor--;
+        
+        return valor;
     }
     
 }

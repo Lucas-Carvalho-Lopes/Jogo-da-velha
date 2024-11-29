455aec1f42fc756c2e9846d5e736e2c52b869f66
Lucas Carvalho <lucas.carvalho.lopes016@gmail.com>
2024-11-29 15:37:02 -0300

    Terminei a codificação do jogo

--- jogodavelha/JogoDaVelha.java
+++ jogodavelha/JogoDaVelha.java
@@ -12,55 +12,85 @@ import java.util.Scanner;
  * @author HOME
  */
 public class JogoDaVelha {
-    
+
     static Scanner scan;
     static Velha velha;
-    
-    public static void main(String[] args){
-        
+
+    public static void main(String[] args) {
+
         scan = new Scanner(System.in);
         velha = new Velha();
-        
+
         boolean ganhou = false;
+        boolean jogadaValida;
         char jogador = 'A';
         int jogada = 0;
-        
-        while(!ganhou){
-            
-            if(jogada % 2 == 0){
-                System.out.println("Vez do jogador 1");
-                jogador = 'X';
-            }else{
-                System.out.println("Vez do jogador 2");
-                jogador = 'O';
+        int linha = 0, coluna = 0;
+
+        while (!ganhou) {
+
+            jogadaValida = false;
+
+            while (!jogadaValida) {
+
+                if (jogada % 2 == 0) {
+                    System.out.println("Vez do jogador 1");
+                    jogador = 'X';
+                } else {
+                    System.out.println("Vez do jogador 2");
+                    jogador = 'O';
+                }
+
+                linha = valor("linha");
+                coluna = valor("coluna");
+
+                if (velha.verificarJogada(linha, coluna)) {
+                    System.out.println("Jogada invalida");
+                } else {
+                    velha.adicionarElemento(linha, coluna, jogador);
+                    jogadaValida = true;
+                    jogada++;
+                }
+
             }
             
+            System.out.println(velha);
             
-            
+            if(velha.verificarVencedor(linha, coluna, jogador) == 1){
+                System.out.println("Jogador 1 venceu!!!");
+                ganhou = true;
+            }else if(velha.verificarVencedor(linha, coluna, jogador) == 2){
+                System.out.println("Jogador 2 venceu!!!");
+                ganhou = true;
+            }else if(jogada > 9){
+                System.out.println("Empate!!!");
+                ganhou = true;
+            }
+
         }
-        
+
     }
-    
-    public static int valor(String tipoValor){
+
+    public static int valor(String tipoValor) {
         int valor = 0;
         boolean valorValido = false;
-        
-        while(!valorValido){
-            
+
+        while (!valorValido) {
+
             System.out.println("Digite o valor da " + tipoValor + ": ");
             valor = scan.nextInt();
-            
-            if(valor >= 1 && valor < 4){
+
+            if (valor >= 1 && valor < 4) {
                 valorValido = true;
-            }else{
+            } else {
                 System.out.println("Valor invalido!");
             }
-            
+
         }
-        
+
         valor--;
-        
+
         return valor;
     }
-    
+
 }
